ad57bb14de5d658edb1e7eacdd7aa28a28d6670b
Ørn <97004381+mateusorn@users.noreply.github.com>
2022-05-20 17:59:11 -0300

    Update StopWin-StopLoss-v2.py

--- StopWin-StopLoss-v2.py
+++ StopWin-StopLoss-v2.py
@@ -12,17 +12,11 @@ from numpy import empty
 from colorama import *
 from termcolor import *
 
-client = Client("KEY", "PRIVATE", {"verify": True, "timeout": 20})
+client = Client("brp2Txdw2Hspn1I8JGzJaIWgOfwfqX6Ad89Om4W5X0IxyBbgfDH6T5Af8vCg1lGE", "ZGybd3qYnHRcvmdQlJbzierxeZuK7TCx7C4DoEi1CbGZIV8SOlTWHlSwkDTvlX2z", {"verify": True, "timeout": 20})
 
 stop_loss = 0.9962 #-0,38% Stop Loss
 stop_gain = 1.0225 #2,25% Stop Gain
 order = list(client.get_open_orders())
-
-# To Do List
-# - Black List Order Ids
-# - Ignore Certain Transactions
-
-mode = 0
 
 #def create_oco(oco_id,oco_buy_price): 
 #    create = client.create_oco_order(
@@ -35,6 +29,7 @@ mode = 0
 #    price=str(stopGainprice))
 #    print(oco_id)
 #    print(oco_buy_price)
+mode = 0
 while (mode < 10):
     if mode == 0:
         os.system("cls")
@@ -53,10 +48,7 @@ while (mode < 10):
             sideOpenOrder = order[0]["side"]
             print(Fore.RED + "[+]" + Fore.GREEN + " Order Found!                               " + Fore.RESET)
             mode = 1
-            
-            
-            
-            
+
     if mode == 1: 
         if len(order) == 1:                         # SE HOUVER APENAS UMA TRANSAÇÃO EM ABERTO
             orderIdOpenOrder = order[0]["orderId"]  # SOLICITA O SIDE DA TRANSAÇÃO
@@ -75,10 +67,6 @@ while (mode < 10):
                 mode = 0 #RETORNA PARA O COMEÇO
                 sleep(10)
                 
-            
-                
-                
-                
         elif len(order) > 1: #SE EXISTIR MAIS DE 1 UMA TRANSAÇÃO ABERTA, FAÇA:
             os.system("cls")
             print(Fore.RED + "[+]" + Fore.GREEN + f" {len(order)} open orders, filtering by [Limit Order - Buy Side]..." + Fore.RESET)
@@ -96,9 +84,6 @@ while (mode < 10):
                 print(Fore.RED + f"[{repeater+1}]" + Fore.GREEN + f" Coin/Amount: {round(float(quantityOpenOrder), 1)} {symbolOpenOrder} | Status: {statusOpenOrder} | Order ID: {orderIdOpenOrder} | Market Price: {priceOpenOrder} | Side: {sideOpenOrder} | USD Amount: {round(float(quantityOpenOrder) * float(priceOpenOrder), 2)}$" + Fore.RESET)
                 repeater+= 1
             
-            
-            
-            
             set = 0
             while len(order) != set: #FAZ PULAR DE NÚMERO EM NÚMERO DE TRANSAÇÕES E SÓ IMPRIMI AS BUYSIDE
                 sideOpenOrder = order[set]["side"] 
@@ -115,52 +100,20 @@ while (mode < 10):
                     set += 1
                     print(Fore.RED + "[+]" + Fore.RED + f" Transaction {orderIdOpenOrder} already blacklisted..." + Fore.RESET)
                     sleep(2)
-                    
-                    
-                    
+
                 elif sideOpenOrder == "BUY":
                     print(Fore.RED + "[+]" + Fore.GREEN + f" We found a buy transaction! Coin: {symbolOpenOrder} | Status: {statusOpenOrder} | Order ID: {orderIdOpenOrder} | Price: {priceOpenOrder}" + Fore.RESET)
                     #create_oco(orderIdOpenOrder, priceOpenOrder)
                     sleep(2)
-                    
-                    
-                    
+
                 else:
                  with open("blacklisted-ids.txt", "a") as myfile: #INSERE O ID DA TRANSAÇÃO SELL SIDE NA BLACKLIST
                     myfile.write("\n" + str(orderIdOpenOrder).replace("\n", ""))
                     print(Fore.RED + "[+]" + Fore.RED + f" Transaction {orderIdOpenOrder} is now on blacklist..." + Fore.RESET)
                     sleep(2)
                     set += 1
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+
         else: #CASO NÃO HAJA ORDENS ABERTAS
             print(Fore.RED + "[+]" + Fore.RED + "No Limit Order")
             sleep(5)
             mode = 0
-            
-        #WIERD DATA SHIT
-        #print(order)
-        #print(type(order))
-        #print(orderIdOpenOrder)
-        #print(sideOpenOrder)
-        #print(len(order))
-        #print(f"FIRST POSITION: {order[0]}")
-        #print(f"SECOND POSITION: {order[1]}")        
